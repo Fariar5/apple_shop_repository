1c346ab9596e808127f7db78e02eddec688373b6
Fariar <far138586@gmail.com>
2025-10-14 14:49:44 +0330

    part4: uploading product & category pages

---
+++ backend/DB/config.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import pg from "pg";
+
+
+dotenv.config()
+
+
+
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
+
+db.connect();
+
+export default db;
